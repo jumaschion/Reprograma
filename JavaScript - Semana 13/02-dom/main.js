742905462005c4da50036f43222cdaf7020cafb4
jumaschion <julia.maschion@hotmail.com>
2018-10-31 09:49:55 -0300

    Exercícios Javascript

--- JavaScript - Semana 13/02-dom/main.js
+++ JavaScript - Semana 13/02-dom/main.js
@@ -14,20 +14,20 @@ input_cpf.addEventListener("focus", function(){
         this.value = ""
     })
 
-    input_cpf.addEventListener("keypress", function(e){
-        e.preventDefault()
-        if("0123456789".indexOf(event.key) != -1 
-        && this.value.indexOf("_") != -1)
+    input_cpf.addEventListener("keydown", function(event) {
+        event.preventDefault()
+        if("0123456789".indexOf(event.key) !== -1
+            && this.value.indexOf("_") !== -1) {
+                this.value = this.value.replace(/_/, event.key)
+                const next_index = this.value.indexOf("_")
+                this.setSelectionRange(next_index, next_index)
+        } else if (event.key === "Backspace") {
+            this.value = this.value.replace(/(\d$)|(\d(?=\D+$))/, "_")
+            const next_index = this.value.indexOf("_")
+            this.setSelectionRange(next_index, next_index)
+        }
+    })
 
-        this.value = this.value.replace(/_/,event.key);
-        const next_index = this.value.indexOf("_")
-        this.setSelectionRange(next_index, next_index)
 
-})
-
-
-
-
-    
 
   
